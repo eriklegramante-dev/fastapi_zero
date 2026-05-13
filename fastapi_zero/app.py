b4bed5bbbc4af29808cb37ff6dafce1b63f25acc
eriklegramante-dev <eriklegramante@gmail.com>
2026-05-13 14:24:44 -0300

    fastapi Users POST and tests added

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,8 +1,28 @@
 from fastapi import FastAPI
+from http import HTTPStatus
+
+from fastapi_zero.schemas import Message, UserSchema, UserPublic, UserDB
 
 app = FastAPI()
 
+database = []
 
-@app.get('/')
+
+@app.get(
+    '/',
+    status_code=HTTPStatus.OK,  # 200
+    response_model=Message,
+)
 def read_root():
     return {'message': 'Olá, mundo!'}
+
+
+@app.post(
+    '/users/', status_code=HTTPStatus.CREATED, response_model=UserPublic
+)  # 201
+def create_user(user: UserSchema):
+    user_with_id = UserDB(**user.model_dump(), id=len(database) + 1)
+
+    database.append(user_with_id)
+
+    return user_with_id
--- tests/test_app.py
+++ tests/test_app.py
@@ -12,3 +12,22 @@ def test_root_deve_retornar_ola_mundo():
 
     assert response.json() == {'message': 'Olá, mundo!'}
     assert response.status_code == HTTPStatus.OK
+
+def test_create_user():
+    client = TestClient(app)
+
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        }
+    )
+
+    assert response.status_code == HTTPStatus.CREATED
+    assert response.json() == {
+        'id': 1,
+        'email': 'alice@example.com',
+        'username': 'alice',
+    }

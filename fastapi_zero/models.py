efdc89a37386e7ef81b53d023175b7844b79efa2
eriklegramante-dev <eriklegramante@gmail.com>
2026-05-22 10:28:23 -0300

    Refactor user management with CRUD operations and integrate database session handling

--- fastapi_zero/models.py
+++ fastapi_zero/models.py
@@ -1,20 +1,24 @@
 from datetime import datetime
 
 from sqlalchemy import func
-from sqlalchemy.orm import Mapped, mapped_column, registry
-
+from sqlalchemy.orm import Mapped, mapped_as_dataclass, mapped_column, registry
 
 table_registry = registry()
 
 
-@table_registry.mapped_as_dataclass
+@mapped_as_dataclass(table_registry)
 class User:
     __tablename__ = 'users'
 
     id: Mapped[int] = mapped_column(init=False, primary_key=True)
     username: Mapped[str] = mapped_column(unique=True)
-    email: Mapped[str] = mapped_column(unique=True)
     password: Mapped[str]
+    email: Mapped[str] = mapped_column(unique=True)
     created_at: Mapped[datetime] = mapped_column(
         init=False, server_default=func.now()
+    )
+
+    # Exercício
+    updated_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now(), onupdate=func.now()
     )
--- tests/test_db.py
+++ tests/test_db.py
@@ -1,4 +1,5 @@
 from dataclasses import asdict
+
 from sqlalchemy import select
 
 from fastapi_zero.models import User
@@ -6,17 +7,19 @@ from fastapi_zero.models import User
 
 def test_create_user(session, mock_db_time):
     with mock_db_time(model=User) as time:
-        new_user = User(username='test', email='test@test', password='secret')
-
+        new_user = User(
+            username='alice', password='secret', email='teste@test'
+        )
         session.add(new_user)
         session.commit()
 
-        user = session.scalar(select(User).where(User.username == 'test'))
+    user = session.scalar(select(User).where(User.username == 'alice'))
 
     assert asdict(user) == {
         'id': 1,
-        'username': 'test',
-        'email': 'test@test',
+        'username': 'alice',
         'password': 'secret',
+        'email': 'teste@test',
         'created_at': time,
+        'updated_at': time,  # Exercício
     }

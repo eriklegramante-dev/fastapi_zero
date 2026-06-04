14656d53ef90854ce50f52dff5ca631e5cfb6e4a
eriklegramante-dev <eriklegramante@gmail.com>
2026-06-04 17:41:17 -0300

    feat: add JWT authentication and password hashing
    - Introduced  schema for token response in .
    - Created  for password hashing and JWT token creation.
    - Updated  to include  and  dependencies.
    - Modified user creation in tests to hash passwords using .
    - Added tests for token generation and JWT decoding in .
    - Enhanced existing tests to validate token retrieval functionality.

--- fastapi_zero/app.py
+++ fastapi_zero/app.py
@@ -1,13 +1,15 @@
 from http import HTTPStatus
 
 from fastapi import Depends, FastAPI, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy import select
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.orm import Session
 
 from fastapi_zero.database import get_session
 from fastapi_zero.models import User
-from fastapi_zero.schemas import Message, UserList, UserPublic, UserSchema
+from fastapi_zero.schemas import Message, UserList, UserPublic, UserSchema, Token
+from fastapi_zero.security import get_password_hash, verify_password, create_access_token
 
 app = FastAPI()
 
@@ -38,7 +40,7 @@ def create_user(user: UserSchema, session: Session = Depends(get_session)):
             )
 
     db_user = User(
-        username=user.username, password=user.password, email=user.email
+        username=user.username, password=get_password_hash(user.password), email=user.email
     )
 
     session.add(db_user)
@@ -68,7 +70,7 @@ def update_user(
 
     try:
         db_user.username = user.username
-        db_user.password = user.password
+        db_user.password = get_password_hash(user.password)
         db_user.email = user.email
         session.commit()
         session.refresh(db_user)
@@ -109,3 +111,27 @@ def read_user__exercicio(
         )
 
     return db_user
+
+@app.post('/token', response_model=Token)
+def login_for_acess_token(form_data: OAuth2PasswordRequestForm = Depends(), session: Session = Depends(get_session)):
+    user = session.scalar(
+        select(User).where(User.email == form_data.username)
+    )
+
+    if not user:
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+            detail='Incorret email or password'
+        )
+
+    if not verify_password(form_data.password, user.password):
+        raise HTTPException(
+            status_code=HTTPStatus.UNAUTHORIZED,
+            detail='Incorret email or password'
+        )
+    
+    access_token = create_access_token(
+        {'sub': user.email}
+    )
+    
+    return {'access_token': access_token, 'token_type': 'Bearer'}
